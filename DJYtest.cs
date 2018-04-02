50a7f46550474780b0e1a360fe412a2259474a4f
HelloCSharpe <linzhenyu_2006@126.com>
2018-04-02 17:22:13 +0800

    Add Test
    添加测试脚本

---
+++ DJYtest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DJYtest : MonoBehaviour {
+
+    // PlayerController pc;
+	GameObject go;
+	// Use this for initialization
+	void Start () {
+		go = this.gameOBjct;//兽兽你改这个代码
+	}
+    int index = 0;
+
+    int count = 7;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
